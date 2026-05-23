766c20ee1daf1a5b24940664692c6cc369f9be1d
adelannabi7-jpg <adelannabi7@gmail.com>
2026-05-23 14:18:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1423,7 +1423,7 @@ class waterDash:
         elif "Data Analysis" in menu_clean:
             st.session_state.notifications = []
             render_data_analysis(self.df, st.session_state.theme)
-        elif "AI Assistant" in menu_clean:
+        elif "🤖 AI Assistant" in menu_clean:
             render_ai(self.df, st.session_state.theme)
 if __name__ == "__main__":
     wDash = waterDash()
