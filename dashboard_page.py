5d8ac417e2e6b0cfb553e47c7001e0c37f7d8d95
adelannabi7-jpg <adelannabi7@gmail.com>
2026-05-23 14:27:15 +0100

    Update dashboard_page.py

--- dashboard_page.py
+++ dashboard_page.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 import altair as alt
 from datetime import datetime, timedelta
-import plotly.express as px
+
 
 
 # ──────────────────────────────────────────────────────────
