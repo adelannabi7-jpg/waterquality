10cf71aab8b28504ae579440fe069356e540226f
adelannabi7-jpg <adelannabi7@gmail.com>
2026-05-23 20:30:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def verify_credentials(username: str, password: str) -> bool:
 
 # Init Firebase only once
 if data_source == "firebase" and not firebase_admin._apps:
-    cred = credentials.Certificate("./lib/firebase-key.json")
+    cred = credentials.Certificate("./lib/firebase-key")
     firebase_admin.initialize_app(cred, {"databaseURL": FIREBASE_URL})
 
 if "notifications_panel_open" not in st.session_state:
